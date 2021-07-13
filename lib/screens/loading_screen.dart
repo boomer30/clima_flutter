662c54e2ed47e0eadea6cb7b01092944e5a24e37
michael.boumansour <michael.boumansour@agilecreatives.net>
2021-07-13 09:36:08 -0400

    Added basic get call

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:clima_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +8,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location currentLocation = Location();
+    await currentLocation.getCurrentLocation();
+    print('Lat: ${currentLocation.latitude} Lon: ${currentLocation.longitude}');
+  }
+
+  @override
+  void initState() {
+    getLocation();
+    print('got here');
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.parse(
+        'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=329a1a6e4f032cef991a734c0012deba');
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
